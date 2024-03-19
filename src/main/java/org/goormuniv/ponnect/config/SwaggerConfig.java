1e7f7541dfcddadaa3b1d3731811f22d100f83be
eun0901 <jk2310400@ajou.ac.kr>
2024-03-20 04:00:34 +0900

    Feat: Swagger 추가

---
+++ src/main/java/org/goormuniv/ponnect/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package org.goormuniv.ponnect.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Ponnect")
+                .description("Ponnect의 API 명세서")
+                .version("1.0.0");
+    }
+}
